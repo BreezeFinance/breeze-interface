89b22574968b9891f61a630f9b6bd86c3ca1c301
lybenson <naivebenson@gmail.com>
2022-10-18 17:29:53 +0800

    chore: add lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': [0],
     '@typescript-eslint/strict-boolean-expressions': [0],
     '@typescript-eslint/no-floating-promises': [0],
-    '@typescript-eslint/return-await': [0]
+    '@typescript-eslint/return-await': [0],
+    'react/react-in-jsx-scope': [0]
   }
 }
