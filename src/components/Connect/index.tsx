8a87a9dba816a9c7c5ff784b2ee8639666e94c14
lybenson <naivebenson@gmail.com>
2022-10-19 19:17:02 +0800

    Updates

--- src/components/Connect/index.tsx
+++
@@ -1,8 +0,0 @@
-function Connect () {
-  return (
-    <>
-    </>
-  )
-}
-
-export default Connect
--- src/constants/tokens.ts
+++
@@ -1,8 +0,0 @@
-import { Token } from '../types/token'
-
-export const tokens: Token[] = [
-  { name: 'APT', symbol: 'WAPT', address: '', icon: '' },
-  { name: 'USDT', symbol: 'USDT', address: '', icon: '' },
-  { name: 'ETH', symbol: 'WETHT', address: '', icon: '' },
-  { name: 'BTC', symbol: 'WBTC', address: '', icon: '' }
-]
