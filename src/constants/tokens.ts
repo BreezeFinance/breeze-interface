79fc187bf07bf3295f7e0e8c5e66d923dcc6ec5e
lybenson <naivebenson@gmail.com>
2022-10-19 18:02:30 +0800

    fix: type error

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,8 +1,8 @@
 import { Token } from '../types/token'
 
 export const tokens: Token[] = [
-  { name: 'APT', symbol: 'WAPT', address: '', logo: '' },
-  { name: 'USDT', symbol: 'USDT', address: '', logo: '' },
-  { name: 'ETH', symbol: 'WETHT', address: '', logo: '' },
-  { name: 'BTC', symbol: 'WBTC', address: '', logo: '' }
+  { name: 'APT', symbol: 'WAPT', address: '', icon: '' },
+  { name: 'USDT', symbol: 'USDT', address: '', icon: '' },
+  { name: 'ETH', symbol: 'WETHT', address: '', icon: '' },
+  { name: 'BTC', symbol: 'WBTC', address: '', icon: '' }
 ]
