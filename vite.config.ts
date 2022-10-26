785e17b668e573cf514fdb7072839c4889a728d2
lybenson <naivebenson@gmail.com>
2022-10-26 16:51:05 +0800

    fix: remove useless code

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import analyze from 'rollup-plugin-analyzer'
+// import analyze from 'rollup-plugin-analyzer'
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,8 +13,7 @@ export default defineConfig({
         'react-vendor': ['react', 'react-dom'],
         'chakra-vendor': ['@chakra-ui/react', '@chakra-ui/icons', '@emotion/react', '@emotion/styled', 'framer-motion']
       }
-    }),
-    analyze()
+    })
   ],
   build: {
     target: 'es2015'
