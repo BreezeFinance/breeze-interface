3282482ac03f3ce224969852cbdf7973fe5efb83
lybenson <naivebenson@gmail.com>
2022-10-26 16:45:09 +0800

    chore: split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import analyze from 'rollup-plugin-analyzer'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()]
+  plugins: [
+    react(),
+    chunkSplitPlugin({
+      strategy: 'default',
+      customSplitting: {
+        'react-vendor': ['react', 'react-dom'],
+        'chakra-vendor': ['@chakra-ui/react', '@chakra-ui/icons', '@emotion/react', '@emotion/styled', 'framer-motion']
+      }
+    }),
+    analyze()
+  ],
+  build: {
+    target: 'es2015'
+  }
 })
